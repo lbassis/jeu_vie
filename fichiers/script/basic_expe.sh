c413c36cc3b14513615155e615e99c6dc11722c2
Lucas Barros De Assis <lbarrosdeas@emi.u-bordeaux1.fr>
2019-05-28 14:28:50 +0200

    tests and results

--- fichiers/script/basic_expe.sh
+++ fichiers/script/basic_expe.sh
@@ -1,7 +1,7 @@
-PARAM="./2Dcomp -n -k vie -i 500 -s 512 " # parametres commun à toutes les executions 
+PARAM="./2Dcomp -n -k vie -i 500 " # parametres commun à toutes les executions 
 OUTPUT="results/basic.data"
 
-echo "iteration;version;conf;grain;temps;" >> $OUTPUT
+echo "iteration;version;conf;grain;size;temps;" >> $OUTPUT
 for iteration in {1..5}
 do
     for version in "seq" "tuile" "tuile_opt"
@@ -10,10 +10,13 @@ do
 	do
 	    for grain in 8 16 32 64
 	    do
-		exp=$PARAM" -g $grain -a $config -v $version"
-		echo $exp
-		echo -n "$iteration;$version;$config;$grain;" >> $OUTPUT
-		$exp 2>> $OUTPUT
+		for size in 512 1024 2048
+		do
+		    exp=$PARAM" -g $grain -a $config -v $version -s $size"
+		    echo $exp
+		    echo -n "$iteration;$version;$config;$grain;$size;" >> $OUTPUT
+		    $exp 2>> $OUTPUT
+		done
 	    done
 	done
     done
--- fichiers/script/opencl_expe.sh
+++ fichiers/script/opencl_expe.sh
@@ -1,18 +1,18 @@
-PARAM="PLATFORM=1 ./2Dcomp -n -k vie -v ocl -i 500 " # parametres commun à toutes les executions 
-OUTPUT="results/opencl.data"
+PARAM="./2Dcomp -n -k vie -v ocl -i 500 " # parametres commun à toutes les executions 
+OUTPUT="results/opencl_global.data"
 
-echo "iteration;version;conf;grain;temps;" >> $OUTPUT
+echo "iteration;version;taille;conf;grain;temps;" >> $OUTPUT
 for iteration in {1..5}
 do
-    for size in 512 4096
+    for size in 512 1024 2048 4096
     do
 	for config in "guns" "random"
 	do
-	    for grain in 16 32
+	    for grain in 8 16 32 64
 	    do
 		exp=$PARAM" -g $grain -a $config -s $size"
 		echo $exp
-		echo -n "$iteration;$size;$config;$grain;" >> $OUTPUT
+		echo -n "$iteration;global;$size;$config;$grain;" >> $OUTPUT
 		$exp 2>> $OUTPUT
 	    done
 	done
